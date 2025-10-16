c2f74c1a6896c12e8db251de4734789db75a6a5e
deepak170601 <deepakreddy1431@gmail.com>
2025-10-16 17:55:12 +0530

    ssl

--- backend/models/db.js
+++ backend/models/db.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // For local development; set to true in production with valid certs
+  },
 });
 
 module.exports = pool;
